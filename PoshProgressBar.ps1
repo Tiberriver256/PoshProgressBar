06ee5206e35cddafe5210b28538656316ba6c428
tiberriver256 <micah.rairdon@gmail.com>
2015-12-14 20:54:01 -0500

    Adding status bar

--- PoshProgressBar.ps1
+++ PoshProgressBar.ps1
@@ -25,35 +25,39 @@ Function New-ProgressBar {
     $syncHash.Runspace = $newRunspace
     $syncHash.Activity = ''
     $syncHash.PercentComplete = 0
+    $syncHash.CurrentOperation = ''
+    $syncHash.AdditionalInfo = ''
     $newRunspace.ApartmentState = "STA" 
     $newRunspace.ThreadOptions = "ReuseThread"           
     $data = $newRunspace.Open() | Out-Null
     $newRunspace.SessionStateProxy.SetVariable("syncHash",$syncHash)           
     $PowerShellCommand = [PowerShell]::Create().AddScript({    
-        [xml]$xaml = @" 
+        [string]$xaml = @" 
         <Window 
             xmlns="http://schemas.microsoft.com/winfx/2006/xaml/presentation" 
             xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" 
             Name="Window" Title="Progress..." WindowStartupLocation = "CenterScreen" 
-            Width = "300" Height = "100" ShowInTaskbar = "True"> 
+            Width = "560" Height="130" SizeToContent="Height" ShowInTaskbar = "True"> 
             <StackPanel Margin="20">
-               <ProgressBar Name="ProgressBar" />
+               <ProgressBar Width="560" Name="ProgressBar" />
                <TextBlock Text="{Binding ElementName=ProgressBar, Path=Value, StringFormat={}{0:0}%}" HorizontalAlignment="Center" VerticalAlignment="Center" />
+               <TextBlock Name="AdditionalInfoTextBlock" Text="" HorizontalAlignment="Center" VerticalAlignment="Center" />
             </StackPanel> 
         </Window> 
 "@ 
-  
-        $reader=(New-Object System.Xml.XmlNodeReader $xaml) 
-        $syncHash.Window=[Windows.Markup.XamlReader]::Load( $reader ) 
+   
+        $syncHash.Window=[Windows.Markup.XamlReader]::parse( $xaml ) 
         #===========================================================================
         # Store Form Objects In PowerShell
         #===========================================================================
-        $xaml.SelectNodes("//*[@Name]") | %{ $SyncHash."$($_.Name)" = $SyncHash.Window.FindName($_.Name)}
+        ([xml]$xaml).SelectNodes("//*[@Name]") | %{ $SyncHash."$($_.Name)" = $SyncHash.Window.FindName($_.Name)}
 
         $updateBlock = {            
             
             $SyncHash.Window.Title = $SyncHash.Activity
             $SyncHash.ProgressBar.Value = $SyncHash.PercentComplete
+            $SyncHash.AdditionalInfoTextBlock.Text = $SyncHash.AdditionalInfo
+            #$SyncHash.Window.MinWidth = $SyncHash.Window.ActualWidth
                        
         }
 
@@ -99,7 +103,6 @@ Function New-ProgressBar {
     return $syncHash
 
 }
- 
 
 function Write-ProgressBar
 {
@@ -109,27 +112,40 @@ function Write-ProgressBar
         $ProgressBar,
         [Parameter(Mandatory=$true)]
         [String]$Activity,
-        [String]$Status,
-        [int]$Id,
         [int]$PercentComplete,
-        [int]$SecondsRemaining,
-        [String]$CurrentOperation,
-        [int]$ParentId,
-        [Switch]$Completed,
-        [int]$SourceID
+        [String]$Status = $Null,
+        [int]$SecondsRemaining = $Null,
+        [String]$CurrentOperation = $Null
     ) 
    
+   Write-Verbose -Message "Setting activity to $Activity"
    $ProgressBar.Activity = $Activity
 
    if($PercentComplete)
    {
-      
+       
+       Write-Verbose -Message "Setting PercentComplete to $PercentComplete"
        $ProgressBar.PercentComplete = $PercentComplete
 
    }
+   
+   if($SecondsRemaining)
+   {
+
+       [String]$SecondsRemaining = "$SecondsRemaining Seconds Remaining"
+
+   }
+   else
+   {
+
+       [String]$SecondsRemaining = $Null
+
+   }
+
+   Write-Verbose -Message "Setting AdditionalInfo to $Status       $SecondsRemaining$(if($SecondsRemaining){ " seconds remaining..." }else {''})       $CurrentOperation"
+   $ProgressBar.AdditionalInfo = "$Status       $SecondsRemaining       $CurrentOperation"
 
 }
-
 
 function Close-ProgressBar
 {
@@ -147,20 +163,26 @@ function Close-ProgressBar
  
 }
 
+
+
 $ProgressBar = New-ProgressBar
 
-1..100 | foreach {Write-ProgressBar -ProgressBar $ProgressBar -Activity "Counting $_ out of 100" -PercentComplete $_; Start-Sleep -Milliseconds 10}
+Write-ProgressBar -ProgressBar $ProgressBar -Activity "Hello" -PercentComplete 50 -CurrentOperation "Counting to 50"
 
-Close-ProgressBar $ProgressBar
-
-[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
-
-$objNotifyIcon = New-Object System.Windows.Forms.NotifyIcon 
-
-$objNotifyIcon.Icon = "C:\Users\tiberriver256\Docueents\GitHub\Tiberriver256.GitHub.io\favicon.ico"
-$objNotifyIcon.BalloonTipIcon = "Error" 
-$objNotifyIcon.BalloonTipText = "A file needed to complete the operation could not be found." 
-$objNotifyIcon.BalloonTipTitle = "File Not Found"
- 
-$objNotifyIcon.Visible = $True 
-$objNotifyIcon.ShowBalloonTip(10000)
+Measure-Command -Expression {
+    $Files = dir $env:USERPROFILE -Recurse
+    $i = 0
+    $Files | foreach {
+    
+                        $i++
+                        Start-Sleep -Milliseconds 10
+    
+                        Write-ProgressBar `
+                                -ProgressBar $ProgressBar `
+                                -Activity "Viewing Files" `
+                                -PercentComplete (($i/$Files.count) * 100) `
+                                -CurrentOperation $_.FullName `
+                                -Status $_.Name `
+                                -SecondsRemaining (100 - $_.count)
+                     }
+}
